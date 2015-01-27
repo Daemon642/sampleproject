93c7cdf6d71709d5da261efb660448c2188f95bb
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-01-27 08:23:05 -0800

    Changes for PaaS Demo Cleanup

--- OPCSupport/ManageJCS/src/nassoleng/oracle/ManageJCSBackups.java
+++ OPCSupport/ManageJCS/src/nassoleng/oracle/ManageJCSBackups.java
@@ -4,6 +4,8 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 public class ManageJCSBackups {
@@ -45,7 +47,6 @@ public class ManageJCSBackups {
         JSONObject jcsBackups = null;
 
         try {
-            System.out.println ("\nGet JCS Backups");
             Client client = ManageJCSUtil.getClient(getUsername(), getPassword());
             WebResource webResource =
                 client.resource(getOpcJCSURL() + getIdentityDomain() + "/" + serviceName + "/backups");
@@ -55,7 +56,7 @@ public class ManageJCSBackups {
                 throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
             } else {
                 String output = response.getEntity(String.class);
-                System.out.println ("\nJCS Backups = " + output);
+                //System.out.println ("\nJCS Backups = " + output);
 
                 jcsBackups = new JSONObject(output);
             }
@@ -67,7 +68,6 @@ public class ManageJCSBackups {
 
     public void deleteJCSBackup(String serviceName, String backupId) {
         try {
-            System.out.println ("\nDelete JCS Backup");
             Client client = ManageJCSUtil.getClient(getUsername(), getPassword());
             WebResource webResource =
                 client.resource(getOpcJCSURL() + getIdentityDomain() + "/" + serviceName + "/backups/" + backupId);
@@ -75,11 +75,40 @@ public class ManageJCSBackups {
 
             if (response.getStatus() != 200) {
                 throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-            } else {
-                String output = response.getEntity(String.class);
-                System.out.println ("\nDelete JCS Backup Output = " + output);
-            }
+            } 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void paasDemoCleanupBackups () {
+        JSONObject jcsBackups = null;
+        JSONArray backupsArray = null;
+        JSONObject jcsBackup = null;
+        String notes = null;
+        String goldBackupId = null;
+
+        System.out.println ("\n**********************************");
+        System.out.println ("Paas Demo Cleanup for JCS Backups");
+        System.out.println ("**********************************\n");
+        
+
+        jcsBackups = getJCSBackups("MyJCS2");
+        try {
+            backupsArray = jcsBackups.getJSONArray("backups");
+            for (int i = 0; i < backupsArray.length(); i++) {
+                jcsBackup = backupsArray.getJSONObject(i);
+                if (jcsBackup.has("notes")) {
+                    notes = jcsBackup.getString("notes");
+                }
+                if (notes != null && notes.equals("Gold Backup for Paas Demo...")) {
+                    goldBackupId = jcsBackup.getString("backupId");
+                } else {
+                    System.out.println ("\nDelete backup " + jcsBackup.getString("backupId"));
+                    deleteJCSBackup("MyJCS2", jcsBackup.getString("backupId"));                                        
+                }
+            }
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
@@ -124,10 +153,11 @@ public class ManageJCSBackups {
             opcConnection.setUsername(args[0]);
             opcConnection.setPassword(args[1]);
             opcConnection.setIdentityDomain(args[2]);
-            opcConnection.getJCSBackups("MyJCS2");
-            opcConnection.getJCSBackupDetails("MyJCS2", "1422313568201");
-            opcConnection.deleteJCSBackup("MyJCS2", "1422313568201");
-            opcConnection.getJCSBackups("MyJCS2");
+            //opcConnection.getJCSBackups("MyJCS2");
+            //opcConnection.getJCSBackupDetails("MyJCS2", "1422313568201");
+            opcConnection.paasDemoCleanupBackups ();
+            //opcConnection.deleteJCSBackup("MyJCS2", "1422313568201");
+            //opcConnection.getJCSBackups("MyJCS2");
         }
     }
 
