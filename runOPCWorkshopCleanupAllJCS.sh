3aabffd78afd3efa9e29a95708d7b306b2c7b3d6
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-03-05 06:03:00 -0800

    Changes

---
+++ runOPCWorkshopCleanupAllJCS.sh
@@ -0,0 +1,15 @@
+# !/bin/sh
+#
+PASSWORD='Welcome2$'
+#
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usdevops40552
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam04611
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam03756
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam82569
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam09373
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam85039
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam08813
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam47561
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam32349
+./runOPCWorkshopJCSCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam15239
+
---
+++ runOPCWorkshopCleanupAllStorage.sh
@@ -0,0 +1,15 @@
+# !/bin/sh
+#
+PASSWORD='Welcome2$'
+#
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usdevops40552
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam04611
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam03756
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam82569
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam09373
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam85039
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam08813
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam47561
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam32349
+./runOPCWorkshopStorageCleanup.sh gse_cloud-admin@oracleads.com ${PASSWORD} usoracleam15239
+
