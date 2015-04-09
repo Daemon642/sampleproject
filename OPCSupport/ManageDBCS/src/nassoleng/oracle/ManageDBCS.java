e3fcaa600e4d7d500a42b579e71dde62934124da
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-04-09 14:42:55 -0700

    DBCS changes

--- OPCSupport/ManageDBCS/src/nassoleng/oracle/ManageDBCS.java
+++ OPCSupport/ManageDBCS/src/nassoleng/oracle/ManageDBCS.java
@@ -92,6 +92,33 @@ public class ManageDBCS {
         return dbcsInstance;
     }
 
+    public String getJobStatus(String jobURL) {
+        JSONObject jobResponse = null;
+        String jobStatus = null;
+
+        try {
+            Client client = ManageDBCSUtil.getClient(getUsername(), getPassword());
+            WebResource webResource =
+                client.resource(jobURL);
+            ClientResponse response = webResource.header("X-ID-TENANT-NAME", getIdentityDomain()).get(ClientResponse.class);
+
+            if (response.getStatus() == 202) {
+                String output = response.getEntity(String.class);
+                System.out.println ("\nJob Status = " + output);
+
+                jobResponse = new JSONObject(output);
+                jobStatus = jobResponse.getString("status");
+            } else if (response.getStatus() == 200) {
+                jobStatus = "Completed";                
+            } else  {
+                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jobStatus;
+    }
+
     public void createAlphaDBCS () {
         ClientResponse response = null;
         String jobURL = null;
@@ -105,6 +132,7 @@ public class ManageDBCS {
                 client.resource(getOpcDBCSURL() + getIdentityDomain());
 
             instanceName = "AlphaDBCS";
+            /* 11G
             String se = new String (
                 "{\n" + 
                 "    \"serviceName\" : \"" + instanceName + "\",\n" + 
@@ -120,6 +148,31 @@ public class ManageDBCS {
                 "        \"usableStorage\" : \"10\",\n" + 
                 "        \"adminPassword\" : \"Alpha2014_\",\n" + 
                 "        \"sid\" : \"ORCL\",\n" + 
+                "        \"failoverDatabase\" : \"no\",\n" + 
+                "        \"backupDestination\" : \"BOTH\",\n" + 
+                "        \"cloudStorageContainer\" : \"Storage-" + getIdentityDomain() + "/AlphaDBCS_SC\",\n" + 
+                "        \"cloudStorageUser\" : \"" + getUsername() + "\",\n" + 
+                "        \"cloudStoragePwd\" : \"" + getPassword() + "\"\n" + 
+                "    }],\n" + 
+                "    \"vmPublicKeyText\" : \"ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArn21PGy1SZ6AYFlztFUL1gv63EXMbSb4qo1SzPAwZgcQXjciU8YsettV81YIFzvIedEn4mhD8ebGKK1k8oYB7HYNsSywbXmqisI+75xY37EZT6ah+cxENmVxmzpOjOYH31wj792tf/WpUUpnN8MdIlTW8uAWNIa6Mz9YhAZ0sJILDOlSNr/rorrGYyYLBtJqbVAZlwEfUSgQTkMwBWK4L7aXOLMDFFAi2oEqsjmT3rWX55YzrwXIMvNXjslen6gXqrdoCeakKMbQ788fQqb1P9hgsmHhkERJfwhgFy+R1RUfPMHdZG7P2vNLUZDd54ROCmj2F852HkertpDMFNMWrQ== oracle@oraclelinux6.localdomain\"\n" + 
+                "}");
+            */
+            String se = new String (
+                "{\n" + 
+                "    \"serviceName\" : \"" + instanceName + "\",\n" + 
+                "    \"version\" : \"12.1.0.2\",\n" + 
+                "    \"level\" : \"PAAS\",\n" + 
+                "    \"description\" : \"Alpha Office Database Cloud Service\",\n" + 
+                "    \"edition\" : \"EE\",\n" + 
+                "    \"subscriptionType\" : \"HOURLY\",\n" + 
+                "    \"shape\" : \"oc3\",\n" + 
+                "\"parameters\" : [\n" + 
+                "    {\n" + 
+                "        \"type\" : \"db\",\n" + 
+                "        \"usableStorage\" : \"10\",\n" + 
+                "        \"adminPassword\" : \"Alpha2014_\",\n" + 
+                "        \"sid\" : \"ORCL\",\n" + 
+                "        \"pdf\" : \"PDB1\",\n" + 
                 "        \"failoverDatabase\" : \"no\",\n" + 
                 "        \"backupDestination\" : \"BOTH\",\n" + 
                 "        \"cloudStorageContainer\" : \"Storage-" + getIdentityDomain() + "/AlphaDBCS_SC\",\n" + 
@@ -146,22 +199,22 @@ public class ManageDBCS {
         }
     }
 
-    public void createDBJCS () {
+    public void createDBCS () {
         JSONObject dbcsInstance = null;
         String status = "In Progress";
         
         System.out.println ("\n***************************");
-        System.out.println ("Create Alpha01JCS Instance");
+        System.out.println ("Create AlphaDBCS Instance");
         System.out.println ("***************************\n");
         
         try {
             createAlphaDBCS ();
-            System.out.println ("Waiting on Create of Alpha01JCS Instance....");
+            System.out.println ("Waiting on Create of AlphaDBCS Instance....");
             Thread.sleep(1000 * 60 * 1); // Sleep for 1 minutes
             while (status.contains("In Progress")) {
-                System.out.println ("Waiting on Create of Alpha01JCS Instance....");
+                System.out.println ("Waiting on Create of AlphaDBCS Instance....");
                 Thread.sleep(1000 * 60 * 1); // Sleep for 1 minutes
-                dbcsInstance = getDBCSInstanceInfo("Alpha01JCS");
+                dbcsInstance = getDBCSInstanceInfo("AlphaDBCS");
                 status = dbcsInstance.getString("status");
             }
             System.out.println ("AlphaDBCS Instance Create finshied....");
@@ -172,6 +225,96 @@ public class ManageDBCS {
         } 
     }
     
+    public String deleteDBCS(String instanceName) {
+        ClientResponse response = null;
+        MultivaluedMap<String, String> headers = null;
+        String jobURL = null;
+
+        try {
+            Client client = ManageDBCSUtil.getClient(getUsername(), getPassword());
+
+            WebResource webResource =
+                client.resource(getOpcDBCSURL() + getIdentityDomain() + "/" + instanceName);
+
+            response =
+                webResource.header("Content-Type", "application/json").header("X-ID-TENANT-NAME", getIdentityDomain()).delete(ClientResponse.class);
+
+            if (response.getStatus() != 202) {
+                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+            } else {
+                headers = response.getHeaders();
+                if (headers != null) {
+                    jobURL = headers.getFirst("Location");
+                    System.out.println ("\nDelete DBCS JobURL = " + jobURL);
+                }
+                String output = response.getEntity(String.class);
+                System.out.println ("\nDelete PaaS Demo DBCS Output = " + output);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jobURL;
+    }
+
+    public void deleteAlphaDBCS() {
+        JSONObject dbcsInstances = null;
+        JSONObject dbcsInstance = null;
+        JSONArray servicesArray = null;
+        String serviceName = "AlphaDBCS";
+        String status = "Terminating";
+        String jobURL = null;
+        
+        try {
+            status = "Terminating";
+            System.out.println ("Delete DBCS " + serviceName);
+            jobURL = deleteDBCS (serviceName);
+            System.out.println ("Waiting on DBCS to be deleted ....");
+            Thread.sleep(1000 * 60 * 2); // Sleep for 1 minutes
+            status = getJobStatus(jobURL);                        
+            while (status.equals("Terminating")) {
+                System.out.println ("Waiting on DBCS to be deleted ....");
+                Thread.sleep(1000 * 60 * 1); // Sleep for 1 minutes
+                status = getJobStatus(jobURL);                        
+            }
+            System.out.println ("DBCS " + serviceName + " has been deleted\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+    }
+
+    public void deleteAllDBCS() {
+        JSONObject dbcsInstances = null;
+        JSONObject dbcsInstance = null;
+        JSONArray servicesArray = null;
+        String serviceName = null;
+        String status = "Terminating";
+        String jobURL = null;
+        
+        dbcsInstances = getDBCSInstances ();
+        try {
+            servicesArray = dbcsInstances.getJSONArray("services");
+            for (int i = 0; i < servicesArray.length(); i++) {
+                dbcsInstance = servicesArray.getJSONObject(i);
+                serviceName = dbcsInstance.getString("service_name");
+                status = "Terminating";
+                System.out.println ("Delete DBCS " + serviceName);
+                jobURL = deleteDBCS (serviceName);
+                System.out.println ("Waiting on DBCS to be deleted ....");
+                Thread.sleep(1000 * 60 * 2); // Sleep for 1 minutes
+                status = getJobStatus(jobURL);                        
+                while (status.equals("Terminating")) {
+                    System.out.println ("Waiting on DBCS to be deleted ....");
+                    Thread.sleep(1000 * 60 * 1); // Sleep for 1 minutes
+                    status = getJobStatus(jobURL);                        
+                }
+                System.out.println ("DBCS " + serviceName + " has been deleted\n");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+    }
 
     public void setUsername(String username) {
         this.username = username;
@@ -226,7 +369,7 @@ public class ManageDBCS {
                 System.out.println ("\n******************");
                 System.out.println ("Create AlphaDBCS");
                 System.out.println ("******************\n");                    
-                opcConnection.createAlphaDBCS();
+                opcConnection.createDBCS();
                 dbcsNames = opcConnection.getDBCSInstanceNames();
                 System.out.println ("\nDBCS Instance Name = " + dbcsNames);                
             } else if (args[3].contains("GetDBCSInstanceInfo")) {
@@ -234,6 +377,16 @@ public class ManageDBCS {
                 System.out.println ("Get AlphaDBCS Instance Details");
                 System.out.println ("******************************\n");                    
                 opcConnection.getDBCSInstanceInfo("AlphaDBCS");
+            } else if (args[3].contains("DeleteAllDBCS")) {
+                System.out.println ("\n***************");
+                System.out.println ("Delete All DBCS");
+                System.out.println ("***************\n");     
+                opcConnection.deleteAllDBCS();
+            } else if (args[3].contains("DeleteAlphaDBCS")) {
+                System.out.println ("\n***************");
+                System.out.println ("Delete AlphaDBCS");
+                System.out.println ("***************\n");     
+                opcConnection.deleteAlphaDBCS();
             }
         }
     }
--- runManageDBCS.sh
+++ runManageDBCS.sh
@@ -5,8 +5,10 @@ then
 	echo "Usage: runManageDBCS.sh <username> <password> <identity domain> <method>" 
         echo "Supported Methods:"
         echo "   GetDBCSInstanceNames"
-        echo "   CreateAlphaDBCS"
         echo "   GetDBCSInstanceInfo"
+        echo "   CreateAlphaDBCS"
+        echo "   DeleteAlphaDBCS"
+        echo "   DeleteAllDBCS"
 	exit 1
 fi
 java -server -classpath ./OPCSupport/ManageDBCS/deploy/ManageDBCS.jar:./OracleCloudStorageAPI/lib/jersey-client-1.13.jar:./OracleCloudStorageAPI/lib/jersey-core-1.13.jar:./OracleCloudStorageAPI/lib/jersey-json-1.13.jar:./OracleCloudStorageAPI/lib/jersey-multipart-1.13.jar:./OracleCloudStorageAPI/lib/jettison-1.1.jar:./OracleCloudStorageAPI/lib/mimepull-1.9.3.jar:./OracleCloudStorageAPI/lib/oracle.cloud.storage.api-13.0.0.jar -Djavax.net.ssl.trustStore=./trustStore.jks nassoleng.oracle.ManageDBCS $1 $2 $3 $4 $5
