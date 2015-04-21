e0dad7c618bea897e1cd87da83e76b3a6f13e32b
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-04-21 13:20:40 -0700

    New accounts

--- runOPCWorkshopCleanupAll_new.sh
+++ runOPCWorkshopCleanupAll_new.sh
@@ -12,53 +12,53 @@ SLEEP_TIME=1m
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle21884 CleanupAccount > Account11Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle21884..."
-echo "*   check output in Account1Cleanup.log"
+echo "*   check output in Account11Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle03294 CleanupAccount > Account12Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle03294..."
-echo "*   check output in Account2Cleanup.log"
+echo "*   check output in Account12Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle45722 CleanupAccount > Account13Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle45722..."
-echo "*   check output in Account3Cleanup.log"
+echo "*   check output in Account13Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle48514 CleanupAccount > Account14Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle48514..."
-echo "*   check output in Account4Cleanup.log"
+echo "*   check output in Account14Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle88327 CleanupAccount > Account15Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle88327..."
-echo "*   check output in Account5Cleanup.log"
+echo "*   check output in Account15Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle54892 CleanupAccount > Account16Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle54892..."
-echo "*   check output in Account6Cleanup.log"
+echo "*   check output in Account16Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle16340 CleanupAccount > Account17Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle16340..."
-echo "*   check output in Account7Cleanup.log"
+echo "*   check output in Account17Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle99246 CleanupAccount > Account18Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle99246..."
-echo "*   check output in Account8Cleanup.log"
+echo "*   check output in Account18Cleanup.log"
 echo "**********************************************************************"
 sleep ${SLEEP_TIME}
 ./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle32870 CleanupAccount > Account19Cleanup.log 2>&1 &
 echo "**********************************************************************"
 echo "* Cleanup of Account started for Identity Domain usoracle32870..."
-echo "*   check output in Account9Cleanup.log"
+echo "*   check output in Account19Cleanup.log"
 echo "**********************************************************************"
---
+++ runOPCWorkshopCreateAll_new.sh
@@ -0,0 +1,64 @@
+# !/bin/sh
+#
+if [ $# -lt 1 ]
+then
+        echo "Usage: runOPCWorkshopCreateAll.sh <password>" 
+        exit 1
+fi
+#
+PASSWORD=$1
+SLEEP_TIME=1m
+#
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle21884 SetupJCSWorkshopAccount > Account11Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle21884..."
+echo "*   check output in Account11Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle03294 SetupJCSWorkshopAccount > Account12Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle03294..."
+echo "*   check output in Account12Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle45722 SetupJCSWorkshopAccount > Account13Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle45722..."
+echo "*   check output in Account13Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle48514 SetupJCSWorkshopAccount > Account14Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle48514..."
+echo "*   check output in Account14Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle88327 SetupJCSWorkshopAccount > Account15Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle88327..."
+echo "*   check output in Account15Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle54892 SetupJCSWorkshopAccount > Account16Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle54892..."
+echo "*   check output in Account16Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle16340 SetupJCSWorkshopAccount > Account17Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle16340..."
+echo "*   check output in Account17Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle99246 SetupJCSWorkshopAccount > Account18Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle99246..."
+echo "*   check output in Account18Create.log"
+echo "**********************************************************************"
+sleep ${SLEEP_TIME}
+./runManageOPC.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle32870 SetupJCSWorkshopAccount > Account19Create.log 2>&1 &
+echo "**********************************************************************"
+echo "* Create of Account started for Identity Domain usoracle32870..."
+echo "*   check output in Account19Create.log"
+echo "**********************************************************************"
