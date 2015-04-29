5454828f894f1b6720398638263e5452a901e998
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-04-28 20:03:28 -0700

    changes

--- GSEScripts/testDB.sh
+++ GSEScripts/testDB.sh
@@ -10,12 +10,12 @@ DB_IP=$1
 echo "************************************************"
 echo "* Copy over needed files to AlphaDBCS image ...."
 echo "************************************************"
-scp -o "StrictHostKeyChecking no" -i labkey checkAlphaUser.* oracle@${DB_IP}:/home/oracle/.
+scp -o "StrictHostKeyChecking no" -i ../labkey checkAlphaUser.* oracle@${DB_IP}:/home/oracle/.
 #
 echo "********************************************"
 echo "* Check that Product Table exists ...."
 echo "********************************************"
-ssh -o "StrictHostKeyChecking no" -i labkey oracle@${DB_IP} "/home/oracle/checkAlphaUser.sh"
+ssh -o "StrictHostKeyChecking no" -i ../labkey oracle@${DB_IP} "/home/oracle/checkAlphaUser.sh"
 #
 echo "**********************************************"
 echo "* OPC Workshop Database check is Complete ...."
