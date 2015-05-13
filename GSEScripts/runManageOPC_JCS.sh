ce8f10fef02b225dbab94cad91ad70d450467255
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-05-13 14:14:30 -0700

    Script changes

--- GSEScripts/runManageOPC_JCS.sh
+++ GSEScripts/runManageOPC_JCS.sh
@@ -11,7 +11,13 @@ then
 	exit 1
 fi
 LOGFILE="logs/jcs/"$3"_"$5".log"
-java -server -classpath ../OPCSupport/ManageOPC/deploy/ManageOPC.jar:../OPCSupport/ManageCompute/deploy/ManageCompute.jar:../OPCSupport/ManageStorage/deploy/ManageStorage.jar:../OPCSupport/ManageJCS/deploy/ManageJCS.jar:../OPCSupport/ManageDBCS/deploy/ManageDBCS.jar:../OracleCloudStorageAPI/lib/jersey-client-1.13.jar:../OracleCloudStorageAPI/lib/jersey-core-1.13.jar:../OracleCloudStorageAPI/lib/jersey-json-1.13.jar:../OracleCloudStorageAPI/lib/jersey-multipart-1.13.jar:../OracleCloudStorageAPI/lib/jettison-1.1.jar:../OracleCloudStorageAPI/lib/mimepull-1.9.3.jar:../OracleCloudStorageAPI/lib/oracle.cloud.storage.api-13.0.0.jar -Djavax.net.ssl.trustStore=../trustStore.jks nassoleng.oracle.ManageOPC $1 $2 $3 $4 $5 $6 > ${LOGFILE} 2>&1 &
+GSE_RUNPATH=
+VBOX_RUNPATH=/u01/OPCWorkshop/lab/
+RUNPATH=$VBOX_RUNPATH
+#RUNPATH=$GSE_RUNPATH
+HTTPPROXY=
+#HTTPPROX=-Dhttp://www-proxy.us.oracle.com:80
+java -server -classpath ${RUNPATH}OPCSupport/ManageOPC/deploy/ManageOPC.jar:${RUNPATH}OPCSupport/ManageCompute/deploy/ManageCompute.jar:${RUNPATH}OPCSupport/ManageStorage/deploy/ManageStorage.jar:${RUNPATH}OPCSupport/ManageJCS/deploy/ManageJCS.jar:${RUNPATH}OPCSupport/ManageDBCS/deploy/ManageDBCS.jar:${RUNPATH}OracleCloudStorageAPI/lib/jersey-client-1.13.jar:${RUNPATH}OracleCloudStorageAPI/lib/jersey-core-1.13.jar:${RUNPATH}OracleCloudStorageAPI/lib/jersey-json-1.13.jar:${RUNPATH}OracleCloudStorageAPI/lib/jersey-multipart-1.13.jar:${RUNPATH}OracleCloudStorageAPI/lib/jettison-1.1.jar:${RUNPATH}OracleCloudStorageAPI/lib/mimepull-1.9.3.jar:${RUNPATH}OracleCloudStorageAPI/lib/oracle.cloud.storage.api-13.0.0.jar -Djavax.net.ssl.trustStore=${RUNPATH}trustStore.jks ${HTTPPROXY} nassoleng.oracle.ManageOPC $1 $2 $3 $4 $5 $6 > ${LOGFILE} 2>&1 &
 echo "**********************************************************************"
 echo "* $5 started for Identity Domain $3..."
 echo "*   check output in ${LOGFILE}"
