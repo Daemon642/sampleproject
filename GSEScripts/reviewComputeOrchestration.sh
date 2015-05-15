75d948d87fd317641a70b9ab38a001a58a4393f3
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-05-15 08:13:43 -0700

    Updated script

--- GSEScripts/reviewComputeOrchestration.sh
+++ GSEScripts/reviewComputeOrchestration.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-        echo "Usage: reviewComputeOrchestration.sh <zone>" 
+        echo "Usage: reviewComputeOrchestration.sh <zone> <passwd>" 
         exit 1
 fi
 #
 ZONE=$1
+PASSWORD=$2
 #
+echo ${PASSWORD} > /u01/OPCWorkshop/lab/GSEScripts/passwd2.txt
+chmod 600 /u01/OPCWorkshop/lab/GSEScripts/passwd2.txt
+nimbula-api -a https://api-${ZONE}.compute.us2.oraclecloud.com -p /u01/OPCWorkshop/lab/GSEScripts/passwd2.txt -u /Compute-usoracle16033/pat.davies@oracle.com list orchestration /Comp
+ute-usoracle16033 -f json
 
-nimbula-api -a https://api-${ZONE}.compute.us2.oraclecloud.com -p /u01/OPCWorkshop/lab/GSEScripts/passwd.txt -u /Compute-usoracle16033/pat.davies@oracle.com list orchestration /Compute-usoracle16033
-# nimbula-api -a https://api-${ZONE}.compute.us2.oraclecloud.com -p /u01/OPCWorkshop/lab/GSEScripts/passwd.txt -u /Compute-usoracle16033/pat.davies@oracle.com list orchestration /Compute-usoracle16033 -f json
+rm /u01/OPCWorkshop/lab/GSEScripts/passwd2.txt
