d96001689ed951dfd449ed3546ae36fc5730bc10
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-05-27 13:49:40 -0700

    changes

--- GSEScripts/setupJCS11_19.sh
+++ GSEScripts/setupJCS11_19.sh
@@ -8,12 +8,12 @@ fi
 #
 PASSWORD=$1
 #
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle21884 z11 SetupJCSWorkshopAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com Oracle12! usoracle03294 z11 SetupJCSWorkshopAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle45722 z11 SetupJCSWorkshopAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle48514 z11 SetupJCSWorkshopAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle88327 z11 SetupJCSWorkshopAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle21884 z11 SetupJCSWorkshopAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle03294 z11 SetupJCSWorkshopAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle45722 z11 SetupJCSWorkshopAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle48514 z11 SetupJCSWorkshopAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle88327 z11 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle54892 z11 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle16340 z11 SetupJCSWorkshopAccount
-./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle99246 z11 SetupJCSWorkshopAccount
+#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle99246 z11 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracle32870 z11 SetupJCSWorkshopAccount
--- GSEScripts/setupJCS1_10.sh
+++ GSEScripts/setupJCS1_10.sh
@@ -10,12 +10,12 @@ PASSWORD=$1
 #
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usdevops40552 z12 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam04611 z11 SetupJCSWorkshopAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam03756 z13 SetupJCSWorkshopAccount - Unable to create DBCS
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam03756 z13 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam82569 z11 SetupJCSWorkshopAccount
 #./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam09373 z13 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam85039 z11 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam08813 z13 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam47561 z11 SetupJCSWorkshopAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam32349 z13 SetupJCSWorkshopAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam15239 z11 SetupJCSWorkshopAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam15239 z11 SetupJCSWorkshopAccount
 
