b7afe66d845be6fcf073bb6d8710b9d47afa8210
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-05-26 17:33:13 -0700

    changes

--- GSEScripts/cleanupJCS1_10.sh
+++ GSEScripts/cleanupJCS1_10.sh
@@ -12,7 +12,7 @@ PASSWORD=$1
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam04611 z11 CleanupAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam03756 z13 CleanupAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam82569 z11 CleanupAccount
-#./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam09373 z13 CleanupAccount
+./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam09373 z13 CleanupAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam85039 z11 CleanupAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam08813 z13 CleanupAccount
 ./runManageOPC_JCS.sh gse_support-admin@oracleads.com ${PASSWORD} usoracleam47561 z11 CleanupAccount
