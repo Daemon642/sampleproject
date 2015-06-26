a11a90409238406a6229d6e5a7cf0eb696f59dc1
Oracle Linux User <oracle@oraclelinux6.localdomain>
2015-06-26 06:32:39 -0700

    changes

--- GSEScripts/runEMHybridSetup.sh
+++ GSEScripts/runEMHybridSetup.sh
@@ -21,7 +21,7 @@ echo "************************************************"
 echo "* Setup ssh for oracle user on OTD VM ...."
 echo "************************************************"
 scp -o "StrictHostKeyChecking no" -i ../labkey ../labkey.pub opc@${OTD_IP}:/home/opc/.
-scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/oracleSudo.txt opc@${JCS_IP}:/home/opc/.
+scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/oracleSudo.txt opc@${OTD_IP}:/home/opc/.
 scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/setupOTD.sh opc@${OTD_IP}:/home/opc/.
 ssh -t -o "StrictHostKeyChecking no" -i ../labkey opc@${OTD_IP} "sudo -i /home/opc/setupOTD.sh"
 #
@@ -30,7 +30,7 @@ echo "* Setup DBCS VM ...."
 echo "************************************************"
 scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/setupDBCS1.sh oracle@${DB_IP}:~/.
 scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/setupDBCS2.sh opc@${DB_IP}:/home/opc/.
-scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/oracleSudo.txt opc@${JCS_IP}:/home/opc/.
+scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/oracleSudo.txt opc@${DB_IP}:/home/opc/.
 scp -o "StrictHostKeyChecking no" -i ../labkey ../EMHybrid/unlockDBSNMP.sql oracle@${DB_IP}:~/.
 ssh -t -o "StrictHostKeyChecking no" -i ../labkey oracle@${DB_IP} "~/setupDBCS1.sh"
 ssh -t -o "StrictHostKeyChecking no" -i ../labkey opc@${DB_IP} "sudo -i /home/opc/setupDBCS2.sh"
